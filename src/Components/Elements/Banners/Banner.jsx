ebe83e0accecee10ddfd708b0595226d19a11258
ZnaReak <libardojesusrengifo129@gmail.com>
2020-10-17 15:45:33 -0430

    banner component

--- src/Components/Elements/Banners/Banner.jsx
+++ src/Components/Elements/Banners/Banner.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Banner (){
-    
+function Banner({
+  title,
+  subtitle = "",
+  image,
+
+  color1 = "f5af19ed",
+  color2 = "f127119a",
+}) {
+  const styles = {
+    background: `linear-gradient(to right, #${color1}, #${color2}), url("${image}")`,
+  };
+  return (
+    <header className="banner-category" style={styles}>
+      <h3 className="banner-title title">{title}</h3>
+      <h5 className="banner-subtitle">{subtitle}</h5>
+    </header>
+  );
 }
 
+Banner.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  color1: PropTypes.string,
+  color2: PropTypes.string,
+};
 export default Banner;
--- src/Components/Pages/Movies/Elements/MoviesList.jsx
+++ src/Components/Pages/Movies/Elements/MoviesList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Movie from "./Movie";
+import Banner from "../../../Elements/Banners/Banner";
 
 const movie = {
   title: "Harry Potter y las Reliquias de la Muerte",
@@ -10,72 +11,94 @@ const movie = {
 export default function MoviesList() {
   return (
     <div className="movies-movies">
-      <div className="columns">
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
+      <div className="movies-category">
+        <Banner
+          title="Género de acción"
+          image="https://image.freepik.com/free-vector/stylish-hexagonal-line-pattern-background_1017-19742.jpg"
+        />
+        <div className="columns">
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
         </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
+      </div>
+
+      <div className="movies-category">
+        <Banner
+          title="Género de romance"
+          image="https://image.freepik.com/free-vector/stylish-hexagonal-line-pattern-background_1017-19742.jpg"
+          color1="000428"
+          color2="004e92a8"
+        />
+        <div className="columns">
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
         </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
-        </div>
-        <div className="column-6 column-sm-12">
-          <Movie {...movie} />
+      </div>
+
+      <div className="movies-category">
+        <Banner
+          title="Género de ficción"
+          image="https://image.freepik.com/free-vector/stylish-hexagonal-line-pattern-background_1017-19742.jpg"
+          color1="FF5F6D"
+          color2="004e92b3"
+        />
+        <div className="columns">
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
+          <div className="column-6 column-sm-12">
+            <Movie {...movie} />
+          </div>
         </div>
       </div>
     </div>
