2959ba2907bcbbe9c63d98f518b219244c2f98d2
ZnaReak <libardojesusrengifo129@gmail.com>
2020-10-18 16:24:41 -0430

    error in the parameter

--- src/Components/Config/apiMovies.js
+++ src/Components/Config/apiMovies.js
@@ -13,4 +13,4 @@ export const MOVIES_GENRERS = [
 export const MOVIE_API_BASE_URL = "https://yts.mx/api/v2/";
 export const MOVIES_ALL = `${MOVIE_API_BASE_URL}list_movies.json`;
 export const MOVIES_GENRERS_URL = (genrer) =>
-  `${MOVIE_API_BASE_URL}list_movies.json?genrer=${genrer}`;
+  `${MOVIE_API_BASE_URL}list_movies.json?genre=${genrer}`;
